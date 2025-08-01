58789b35f41daf7eb4158fcecabf338f4ea924b3
hffariel <farielclaire@gmail.com>
2025-08-01 17:22:31 +0800

    f

--- variables.tf
+++ variables.tf
@@ -1,12 +1,9 @@
 variable "celerdata_cluster_name" {
   description = "The name of the celerdata cluster to be deployed."
   type        = string
-  required    = true
 }
 
 variable "celerdata_service_account_email" {
   description = "The email of the celerdata service account."
   type        = string
-  required    = true
 }
-
