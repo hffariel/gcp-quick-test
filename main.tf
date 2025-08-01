84541d9ba2d21439c63da9d7164e28fd9fac6fdd
hffariel <farielclaire@gmail.com>
2025-08-01 15:47:48 +0800

    f

--- main.tf
+++ main.tf
@@ -7,8 +7,34 @@ provider "google" {
   }
 }
 
+resource "random_string" "celerdata_recource_suffix" {
+  special = false
+  upper   = false
+  lower   = true
+  length  = 5
+}
+
 resource "google_project_service" "celerdata_enabled_services" {
   for_each           = toset(local.celerdata_required_gcp_services)
   service            = each.value
   disable_on_destroy = false
+}
+
+locals {
+  project_id = split("/", google_project_service.celerdata_enabled_services[0].id)[0]
+  celerdata_created_resource_common_prefix = "cd-${var.celerdata_cluster_name}-${random_string.celerdata_recource_suffix.result}"
+}
+
+locals {
+  celerdata_created_subnet_cird   = "10.1.0.0/16"
+  celerdata_cluster_network_tag   = "${local.celerdata_created_resource_common_prefix}-network-tag"
+  celerdata_required_gcp_services = [
+    "compute.googleapis.com",
+    "storage.googleapis.com",
+    "container.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "sqladmin.googleapis.com",
+    "run.googleapis.com"
+  ]
 }
--- storage.tf
+++ storage.tf
@@ -15,7 +15,3 @@ resource "google_storage_bucket" "celerdata_created_data_bucket" {
     google_project_service.celerdata_enabled_services
   ]
 }
-
-output "project_from_id" {
-  value = provider::google::project_from_id(google_compute_network.celerdata_created_network.self_link)
-}
